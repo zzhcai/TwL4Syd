8807124f17f6260ea1499eacfa20fe4e74f9e8e5
zzhcai <87593086+zzhcai@users.noreply.github.com>
2022-03-17 21:37:46 +1100

    Trivials

--- utils.py
+++ utils.py
@@ -1,28 +1,34 @@
 import json
 
+
 class ConciseTweet:
+
     """
     Concise tweet information,
     only containing location coordinates, and language code
     
     """
-    
+
     def __init__(self, line):
         """
         :param line: a line in the twitter data file
         :type line: JSON String document
         
         """
+
         self.coord = self.lang = None
-        
+
         tweet = self.truncated(line)
         if tweet and 'doc' in tweet:
             doc = tweet['doc']
-            if 'coordinates' in doc and doc['coordinates'] and 'coordinates' in doc['coordinates']:
+            if 'coordinates' in doc and doc['coordinates'] \
+                and 'coordinates' in doc['coordinates']:
                 self.coord = doc['coordinates']['coordinates']
-            elif 'geo' in doc and doc['geo'] and 'coordinates' in doc['geo']:
+            elif 'geo' in doc and doc['geo'] and 'coordinates' \
+                in doc['geo']:
                 self.coord = doc['geo']['coordinates'][::-1]
-            self.lang = doc['lang'] if 'lang' in doc and isinstance(doc['lang'], str) else None
+            self.lang = (doc['lang'] if 'lang' in doc
+                         and isinstance(doc['lang'], str) else None)
 
     @staticmethod
     def truncated(line):
@@ -33,6 +39,7 @@ class ConciseTweet:
         :rtype: dict
         
         """
+
         if line.startswith('{"id"'):
             if line.endswith(',\n'):
                 line = line[:-2]
