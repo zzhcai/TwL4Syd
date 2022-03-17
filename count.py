788ec76193192ca9981b8fc65292890f14b4725f
zzhcai <87593086+zzhcai@users.noreply.github.com>
2022-03-18 02:57:29 +1100

    Load grid shape data, cast tweets

--- count.py
+++ count.py
@@ -1,31 +1,82 @@
 import argparse
-from mpi4py import MPI
+import json
 
-from utils import ConciseTweet
+from mpi4py import MPI
+from utils import count, output, output_mpi   # utils.py
 
 # Command-line arguments
-parser = argparse.ArgumentParser(description='Twitter Geospatial Language Analysis for Sydney')
-parser.add_argument('--grid_path', type=str, default=r'data/sydGrid.json',
-                    help='Path to the grid shape file')
-parser.add_argument('--twitter_path', type=str, default=r'data/largeTwitter.json',
+
+parser = \
+    argparse.ArgumentParser(description='Twitter Language Geospatial Analysis for Sydney'
+                            )
+parser.add_argument('--twitter_path', type=str,
+                    default=r'data/largeTwitter.json',
                     help='Path to the twitter data file')
+parser.add_argument('--batch_size_per_message', type=int, default=50,
+                    help='The number of tweets bared per message')
+
 args = parser.parse_args()
+
+GRID_PATH = r'data/sydGrid.json'   # path to the grid shape file
+N_GRID = 16   # number of grids
+
 
 def main():
 
     comm = MPI.COMM_WORLD
     rank = comm.Get_rank()
     size = comm.Get_size()
-    
-    with open(args.twitter_path, 'r') as ft:
-        with open(args.grid_path, 'r') as fg:
-            
-            for i, line in enumerate(ft):
-                # round-robin job allocation to each process
-                if i % size == rank:
-                    c_tweet = ConciseTweet(line)
-                    if c_tweet.coord and c_tweet.lang:   # not null
-                        pass
+
+    cell_tweetlang_dict, lang_tweet_dict = {}, {}
+    with open(GRID_PATH, 'r') as fg:
+        grids = [(f['geometry']['coordinates'][0])[:4] for f in
+                 json.load(fg)['features']]
+
+    # MPI not needed
+    if size == 1:
+
+        pass   # @TODO
+
+    else:
+
+        # rank-0 task distributor
+        if rank == 0:
+
+            with open(args.twitter_path, 'r') as ft:
+                to = 1
+                batch = []
+                for i, line in enumerate(ft):
+                    batch.append(line)
+                    if (i + 1) % args.batch_size_per_message == 0:   # full batch
+                        comm.send(batch, dest=to, tag=31)
+                        to = (to + 1 if to < size - 1 else 1)
+                        batch.clear()
+
+                # last batch of tweets
+                if batch:
+                    comm.send(batch, dest=to, tag=31)
+
+                # stop send
+                for i in range(1, size):
+                    comm.send([], dest=i, tag=31)
+
+        # other workers
+        else:
+
+            while True:
+                batch = comm.recv(source=0, tag=31)
+                if batch:
+                    (cell_tweetlang_dict, lang_tweet_dict) = count(rank,
+                            grids, batch, cell_tweetlang_dict,
+                            lang_tweet_dict)
+                else:   # []
+                    break
+
+        # gathering
+
+        comm.gather(tuple((cell_tweetlang_dict, lang_tweet_dict)), root=0)
+        output_mpi(cell_tweetlang_dict, lang_tweet_dict)
+
 
 if __name__ == '__main__':
     main()
--- utils.py
+++ utils.py
@@ -35,18 +35,53 @@ class ConciseTweet:
         """
         :param line: a line in the twitter data file
         :type line: JSON String document
-        :return: a formatted Python object if is a tweet
+        :return: a formatted Python object if is a tweet, otherwise None
         :rtype: dict
         
         """
 
         if line.startswith('{"id"'):
-            if line.endswith(',\n'):
+            line = line.strip()
+            if line.endswith(','):
+                line = line[:-1]
+            elif line.endswith(']}'):
                 line = line[:-2]
-            elif line.endswith(']}\n'):
-                line = line[:-3]
             try:
                 return json.loads(line)
             except json.decoder.JSONDecodeError:
                 pass
         return None
+
+
+# @TODO: Remove rank
+def count(rank, grids, batch, cell_tweetlang_dict, lang_tweet_dict):
+    """
+    Accumulator folds by new batch of tweets
+    
+    :param grids: coordinates of grid cells
+    :param batch: a list of str tweets
+    :param cell_tweetlang_dict: {cell: (#tweets, #lang)}
+    :param lang_tweet_dict: {lang: #tweets}
+    :return: 2 updated dictionaries
+    
+    """
+    for line in batch:
+        c_tweet = ConciseTweet(line)
+        if c_tweet.coord and c_tweet.lang:
+            # @TODO
+            print(rank, c_tweet.coord, c_tweet.lang)
+    
+    return cell_tweetlang_dict, lang_tweet_dict
+
+def output():
+    """
+    
+    
+    """
+
+
+def output_mpi(cell_tweetlang_dict, lang_tweet_dict):
+    """
+    
+    
+    """
